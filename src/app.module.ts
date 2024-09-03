4e3570b399de3afb73bd21dfdfac6255900b1ae9
Carboncito <carboncito760@gmail.com>
2024-09-03 14:59:36 -0600

    set up base path

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { OmdbModule } from './omdb/omdb.module';
+import { AppController } from './app.controller';
 
 @Module({
   imports: [
@@ -33,7 +34,7 @@ import { OmdbModule } from './omdb/omdb.module';
     UserModule,
     OmdbModule,
   ],
-  controllers: [],
+  controllers: [AppController],
   providers: [],
 })
 export class AppModule {}
